cb23794e653e81acf9d1a6d83a3400db1fc492fa
limkethyaravy <limkethyaravycybermail@gmail.com>
2026-08-26 21:52:21 +0700

    modify eloquent

--- backend/app/Models/Rooms.php
+++ backend/app/Models/Rooms.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder;
 
 class Rooms extends Model
 {
